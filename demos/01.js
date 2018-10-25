6e3a83eeb614e37a2026f44e5a6d1733c08ab4ae
zhangyuhong <zhangyuhong@didichuxing.com>
2018-10-25 20:52:29 +0800

    增加文件上传功能

--- demos/01.js
+++ demos/01.js
@@ -1,6 +1,15 @@
+/**
+ * web app 的功能
+ * ctx.cookies用来读写cookie
+ * koa-body模块可以用来从 POST 请求的数据体里面提取键值对。
+ */
+
 const Koa=require("koa")
-const app=new Koa()
 const koaBody=require("koa-body")// koa-body模块可以用来从 POST 请求的数据体里面提取键值对。
+const os=require("os")
+const path=require("path")
+
+const app=new Koa()
 
 // ctx.cookies用来读写 Cookie。
 // const main =function(ctx){
@@ -19,6 +28,25 @@ const koaBody=require("koa-body")// koa-body模块可以用来从 POST 请求的
 // }
 // app.use(koaBody())// 注意调用的方法
 
+// 文件上传:koa-body模块还可以用来处理文件上传
+const main=async function(ctx){
+    const tmpdir = os.tmpdir()
+    const filePaths=[];
+    const files=ctx.request.body.file || {}
+    for(let key in files){
+        const file=files[key]
+        const filePath=path.join(tmpdir,filename);
+        const reader=fs.createReadStream(file.path)
+        const writer=fs.createWriteStream(filePath)
+        reader.pipe(writer)
+        filePaths.push(filePath)
+    }
+    ctx.body=filePaths
+    
+}
+
+
+app.use(koaBody({multipart:true}))
 
 app.listen(3030,()=>{
     console.log("hello koa")
